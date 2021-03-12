b4cbafcdd525f36f464db050c4e28053b89af44c
krylovam <krylova.maria.2629@gmail.com>
2021-03-12 18:16:13 +0300

    update solution

--- src/Complex.kt
+++ src/Complex.kt
@@ -1,30 +1,46 @@
-class Complex (val re : Double, val im : Double) {
-    operator fun plus(otherValue: Complex): Complex{
+class Complex constructor(re : Double, im : Double)
+{
+    var re : Double
+    var im : Double
+    init
+    {
+        this.re = re
+        this.im = im
+    }
+
+    operator fun plus(otherValue: Complex): Complex
+    {
         return Complex(re + otherValue.re, im + otherValue.im)
     }
 
-    operator fun plus(otherValue: Double): Complex{
+    operator fun plus(otherValue: Double): Complex
+    {
         return Complex(re + otherValue, im)
     }
 
-    operator fun minus(otherValue: Complex): Complex{
+    operator fun minus(otherValue: Complex): Complex
+    {
         return Complex(re - otherValue.re, im - otherValue.im)
     }
 
-    operator fun minus(otherValue: Double): Complex{
+    operator fun minus(otherValue: Double): Complex
+    {
         return Complex(re - otherValue, im)
     }
 
-    operator fun times(otherValue: Complex): Complex{
+    operator fun times(otherValue: Complex): Complex
+    {
         return Complex(re * otherValue.re - im * otherValue.im,
                        re * otherValue.im + im * otherValue.re)
     }
 
-    operator fun times(otherValue: Double): Complex{
+    operator fun times(otherValue: Double): Complex
+    {
         return Complex(re * otherValue,  im * otherValue)
     }
 
-    override fun toString() : String {
+    override fun toString() : String
+    {
         var result = "$re"
         if (im == 0.0) {
             return result
--- src/Matrix.kt
+++ src/Matrix.kt
@@ -1,18 +1,30 @@
-class Matrix(
-    var height: Int, var width: Int,
-    private var data: Array<Array<Complex>> = Array(height) {
-        Array(width, { i -> Complex(0.0, 0.0) })
+class Matrix constructor(height : Int, width : Int)
+{
+    var height : Int
+    var width : Int
+    var data : Array < Array < Complex > >
+    init
+    {
+        this.height = height
+        this.width = width
+        this.data = Array(height) { Array(width) { i -> Complex(0.0, 0.0) } }
     }
-){
-    operator fun get(row : Int, col : Int): Complex{
+    constructor(height: Int, width: Int, data : Array < Array < Complex > >) : this (height, width)
+    {
+        this.data = data
+    }
+    operator fun get(row : Int, col : Int): Complex
+    {
         return data[row][col]
     }
 
-    operator fun set(row : Int, col : Int, value: Complex) {
+    operator fun set(row : Int, col : Int, value: Complex)
+    {
         data[row][col] = value
     }
 
-    fun transpose(): Matrix {
+    fun transpose(): Matrix
+    {
         val resultMatrix = Matrix(width, height)
         for (i in 0 until height) {
             for (j in 0 until width) {
@@ -22,7 +34,8 @@ class Matrix(
         return resultMatrix
     }
 
-    operator fun plus( otherMatrix: Matrix): Matrix {
+    operator fun plus( otherMatrix: Matrix): Matrix
+    {
         if (width != otherMatrix.width || height != otherMatrix.height) {
             throw IncompatibleMatricesException("Matrices have different number of rows or columns")
         }
@@ -35,7 +48,8 @@ class Matrix(
         return resultMatrix
     }
 
-    operator fun times( otherMatrix : Matrix): Matrix {
+    operator fun times( otherMatrix : Matrix): Matrix
+    {
         if (height != otherMatrix.width || width != otherMatrix.height) {
             throw IncompatibleMatricesException("Matrices have incompatible number of rows or columns")
         }
@@ -50,7 +64,8 @@ class Matrix(
         return resultMatrix
     }
 
-    override fun toString(): String {
+    override fun toString(): String
+    {
         val result = StringBuilder()
         for (i in 0 until height) {
             for (j in 0 until width) {
@@ -60,7 +75,6 @@ class Matrix(
         }
         return result.toString()
     }
-
 }
 
 class IncompatibleMatricesException(message:String): Exception(message)
